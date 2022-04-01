bcaf227629269f110d08215f3a21835d272d94ca
Carlos André Antunes <c4rlo4@gmail.com>
2022-04-01 11:16:15 -0300

    criação da classe place

---
+++ projeto_sistema_contabil/model/place.py
@@ -0,0 +1,7 @@
+class Place:
+    
+    def _init_(self, address, created_by, created_at, updated_at):
+        self.address = address
+        self.created_by = created_by
+        self.created_at = created_at
+        self.updated_at = updated_at
