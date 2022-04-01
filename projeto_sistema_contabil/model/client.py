dfa8fa6ccf93fa3f562cdb1b5a53f031ffabe1c3
Carlos André Antunes <c4rlo4@gmail.com>
2022-04-01 11:06:11 -0300

    making client and MVC structure

---
+++ projeto_sistema_contabil/model/client.py
@@ -0,0 +1,11 @@
+class Client:
+
+    def __init__(self, id, full_name, nas, created_by, created_at, updated_at):
+        self.id = id
+        self.full_name = full_name
+        self.nas = nas
+        self.created_by = created_by
+        self.created_at = created_at
+        self.updatred_at = updatred_at
+
+
